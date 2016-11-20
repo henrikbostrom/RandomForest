256e9073730df24073b3dd5051723461bc14e682
ahmedfaroukyehia <farouk@novelari.com>
2016-11-20 13:26:31 +0200

    function minification for survival

--- src/survival.jl
+++ src/survival.jl
@@ -1,31 +1,46 @@
-function generate_trees(Arguments::Tuple{LearningMethod{Survival},Array{Int,1},Int,Int})
+function generate_trees(Arguments::Tuple{LearningMethod{Survival},Array{Int,1},Int,Int};curdata=globaldata, randomoobs=[], varimparg = true)
     method,classes,notrees,randseed = Arguments
-    s = size(globaldata,1)
+    s = size(curdata,1)
     srand(randseed)
-    trainingdata = globaldata
+    trainingdata = curdata
     trainingrefs = collect(1:size(trainingdata,1))
-    trainingweights = trainingdata[:WEIGHT].data
+    trainingweights = trainingdata[:WEIGHT]
     regressionvalues = []
-    timevalues = trainingdata[:TIME].data
-    eventvalues = trainingdata[:EVENT].data
-    oobpredictions = Array(Array{Float64},size(trainingdata,1))
-    for i = 1:size(trainingdata,1)
+    timevalues = trainingdata[:TIME]
+    eventvalues = trainingdata[:EVENT]
+    oobpredictions = Array(Array{Float64,1},size(curdata,1))
+    for i = 1:size(curdata,1)
         oobpredictions[i] = zeros(3)
     end
+    randomclassoobs = Array(Any,size(randomoobs,1))
+    for i = 1:size(randomclassoobs,1)
+        oobref = randomoobs[i]
+        c = 1
+        while oobref > size(trainingrefs[c],1)
+            oobref -= size(trainingrefs[c],1)
+            c += 1
+        end
+        randomclassoobs[i] = (c,oobref)
+    end
     # starting from here till the end of the function is duplicated between here and the Classifier and Regressor dispatchers
-    variables, types = get_variables_and_types(globaldata)
+    variables, types = get_variables_and_types(curdata)
     modelsize = 0
+    noirregularleafs = 0
     missingvalues, nonmissingvalues = find_missing_values(method,variables,trainingdata)
     newtrainingdata = transform_nonmissing_columns_to_arrays(method,variables,trainingdata,missingvalues)
     model = Array(TreeNode,notrees)
+    oob = Array(Array,notrees)
     variableimportance = zeros(size(variables,1))
     for treeno = 1:notrees
         sample_replacements_for_missing_values!(method,newtrainingdata,trainingdata,variables,types,missingvalues,nonmissingvalues)
-        model[treeno], treevariableimportance, noleafs, noirregularleafs = generate_tree(method,trainingrefs,trainingweights,regressionvalues,timevalues,eventvalues,newtrainingdata,variables,types,oobpredictions,varimp = true)
+        model[treeno], treevariableimportance, noleafs, treenoirregularleafs, oob[treeno] = generate_tree(method,trainingrefs,trainingweights,regressionvalues,timevalues,eventvalues,newtrainingdata,variables,types,oobpredictions,varimp = true)
         modelsize += noleafs
-        variableimportance += treevariableimportance
+        noirregularleafs += treenoirregularleafs
+        if (varimparg)
+            variableimportance += treevariableimportance
+        end
     end
-   return (model,oobpredictions,variableimportance)
+    return (model,oobpredictions,variableimportance,modelsize,noirregularleafs,randomclassoobs,oob)
 end
 
 function find_missing_values(method::LearningMethod{Survival},variables,trainingdata)
@@ -137,11 +152,12 @@ function generate_tree(method::LearningMethod{Survival},trainingrefs,trainingwei
             oobpredictions[trainingref] += [1,oobprediction,oobprediction^2]
         end
     end
-    if varimp
-        return model, variableimportance, noleafs, noirregularleafs, zeroweights
-    else
-        return model, noleafs, noirregularleafs, zeroweights
-    end
+    # if varimp
+    #     return model, variableimportance, noleafs, noirregularleafs, zeroweights
+    # else
+    #     return model, noleafs, noirregularleafs, zeroweights
+    # end
+    return model, variableimportance, noleafs, noirregularleafs, zeroweights
 end
 
 ## function make_loo_prediction(tree,testdata,exampleno,prediction)
