5d2d3bbc23cbbad020c366138ec39f60ef2ad899
Amgad <amgad@peerialism.com>
2016-12-01 07:14:22 +0200

    added read the docs documentation

--- docs/make.jl
+++
@@ -1,8 +0,0 @@
-using Documenter, RandomForest
-
-makedocs(doctest=false)
-
-deploydocs(deps=Deps.pip("pygments", "mkdocs", "mkdocs-material"),
-           repo="github.com/henrikbostrom/RandomForest.git", #TODO repo name could change
-           julia="0.4",
-           osname="linux")
